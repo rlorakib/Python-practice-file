010df622472999de3af741069a784fae028fe439
Md.Rakib hossen <rlorakib2002@gmail.com>
2022-06-17 14:42:46 +0600

    Update 26.Python User input.py

--- 26.Python User input.py
+++ 26.Python User input.py
@@ -1,4 +1,18 @@
 #Python user input
-
+"""
 username = input('Enter username:')
 print('Username is:',username)
+
+user = input('Enter your number :')
+print(int(user))
+
+
+num1 = int(input('Enter first number:'))
+num2 = int(input('Enter second number:'))
+
+mul = (num1 * num2)
+
+print('Multiplication is :',mul)
+"""
+
+print('rakib','vlogger','channel',sep='**')
